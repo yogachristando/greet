3d06e81a845a53ec56742ddc68c2e1d0daefe028
yogachristando <yogachristando@gmail.com>
2024-05-03 19:11:01 +0700

    Programming Variable

---
+++ variable.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+const namaPelanggan = 'Jordan'
+const umur = 32
+const sudahMenikah = true
+const barangOrderan = ['baju', 'celana', 'sepatu']
+const alamatPelanggan = {
+    namaJalan: 'jl. setiabudi',
+    nomorJalan: 5 
+
+
+}
+alamatPelanggan
+console.log(`Nama: ${namaPelanggan}`);
+console.log(`Umur: ${umur}`);
+console.log(`Sudah Menikah: ${sudahMenikah}`);
+console.log(`Barang Orderan: ${barangOrderan}`);
+console.log(`Alamat Pelanggan: ${JSON.stringify(alamatPelanggan, null, 2)}`);
+
+
+
+const angka = [1, 2, 3, 4]
+console.log (angka);
+
+angka.push(5);
+console.log (angka);
+
+let nama = null;
+
+console.log(`nama: ${nama}`);
+
+const tgl = new Date ();
+console.log(tgl.getFullYear());
+
+
